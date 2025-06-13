62f5965ab60d4c8d6ce0736bc45154364023de87
mepnotme <62350991+mepnotme@users.noreply.github.com>
2025-06-13 16:58:29 +0200

    0.0.1

--- assets/js/binary.js
+++ assets/js/binary.js
@@ -1,4 +1,4 @@
-// meta={"simUrl":"https://trg-arcade.userpxt.io/v2.0.56/---simulator","cdnUrl":"https://cdn.makecode.com","version":"0.0.0","target":"arcade","targetVersion":"2.0.56"}
+// meta={"simUrl":"https://trg-arcade.userpxt.io/v2.0.56/---simulator","cdnUrl":"https://cdn.makecode.com","version":"0.0.1","target":"arcade","targetVersion":"2.0.56","repo":"mepnotme/naves-espasiales"}
 // total=2200645 new=23.25% cached=72.53% other=4.22%
 (function (ectx) {
 'use strict';
@@ -541,7 +541,7 @@ const pxsim_pxtrt = pxsim.pxtrt;
 const pxsim_numops = pxsim.numops;
 
 
-function _main___P23760(s) {
+function _main___P23783(s) {
 let r0 = s.r0, step = s.pc;
 s.pc = -1;
 
@@ -1083,13 +1083,13 @@ switch (step) {
     return leave(s, r0)
   default: oops()
 } } }
-_main___P23760.info = {"start":0,"length":0,"line":0,"column":0,"endLine":0,"endColumn":0,"fileName":"pxt_modules/arcade-background-scroll/main.ts","functionName":"<main>","argumentNames":[]}
-_main___P23760.continuations = [  ]
+_main___P23783.info = {"start":0,"length":0,"line":0,"column":0,"endLine":0,"endColumn":0,"fileName":"pxt_modules/arcade-background-scroll/main.ts","functionName":"<main>","argumentNames":[]}
+_main___P23783.continuations = [  ]
 
-function _main___P23760_mk(s) {
+function _main___P23783_mk(s) {
     checkStack(s.depth);
     return {
-        parent: s, fn: _main___P23760, depth: s.depth + 1,
+        parent: s, fn: _main___P23783, depth: s.depth + 1,
         pc: 0, retval: undefined, r0: undefined, overwrittenPC: false, lambdaArgs: null,
   tmp_0: undefined,
   tmp_1: undefined,
@@ -60707,5 +60707,5 @@ const sprites_BaseSpriteSayRenderer__C2273_VT = mkVTable({
 
 const breakpoints = setupDebugger(1, ["transparency16___4751","projectile___4748","nave___4749","nave2___4747","bonus___4746"])
 
-return _main___P23760
+return _main___P23783
 })
